fe99ffb76c562c15a2a5b5453b3ea3615cf9fe58
arthur999999 <arthurhono.contato@gmail.com>
2023-03-07 17:20:29 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        gato gatito = new gato();
+        System.out.println(gatito);
+        /*int a = 3;
+        int b = 5;
+        System.out.println("Hello World!" + (a + b) );*/
+    }
+}
